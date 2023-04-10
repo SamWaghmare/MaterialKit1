c3b102d9a113b090c40db1d9f9e60078e04b6ba5
Waghmare <Sanket.Waghmare@carrier.com>
2023-04-10 16:09:53 -0400

    Update2

--- src/pages/customers.js
+++ src/pages/customers.js
@@ -15,20 +15,6 @@ const now = new Date();
 
 const data = [
   {
-    id: '5e887ac47eed253091be10cb',
-    address: {
-      city: 'Cleveland',
-      country: 'USA',
-      state: 'Ohio',
-      street: '2849 Fulton Street'
-    },
-    avatar: '/assets/avatars/avatar-carson-darrin.png',
-    createdAt: subDays(subHours(now, 7), 1).getTime(),
-    email: 'carson.darrin@devias.io',
-    name: 'Carson Darrin',
-    phone: '304-428-3097'
-  },
-  {
     id: '5e887b209c28ac3dd97f6db5',
     address: {
       city: 'Atlanta',
